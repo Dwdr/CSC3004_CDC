ad2fcbd6da95c8737befb4bb9d8de87559adb46a
Darren <2101196@sit.singaporetech.edu.sg>
2023-06-21 01:52:29 +0800

    Currently unable to get through error in description
    1/1 [==============================] - 2s 2s/step
    127.0.0.1 - - [21/Jun/2023 01:45:21] "POST /process-image HTTP/1.1" 200 -
    1/1 [==============================] - 3s 3s/step
    WARNING:tensorflow:5 out of the last 5 calls to <function Model.make_predict_function.<locals>.predict_function at 0x17f3fb9c0> triggered tf.function retracing. Tracing is expensive and the excessive number of tracings could be due to (1) creating @tf.function repeatedly in a loop, (2) passing tensors with different shapes, (3) passing Python objects instead of tensors. For (1), please define your @tf.function outside of the loop. For (2), @tf.function has reduce_retracing=True option that can avoid unnecessary retracing. For (3), please refer to https://www.tensorflow.org/guide/function#controlling_retracing and https://www.tensorflow.org/api_docs/python/tf/function for  more details.
    1/1 [==============================] - ETA: 0s127.0.0.1 - - [21/Jun/2023 01:45:22] "POST /process-image HTTP/1.1" 200 -

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
-from dotenv import load_dotenv
-from keras.models import load_model
-from keras.optimizers import SGD
+from flask import Flask, render_template, request, jsonify
+# from keras.models import load_model
+# from keras.optimizers import SGD
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.optimizers import SGD
 
 import base64
 import boto3
@@ -10,6 +12,34 @@ import numpy as np
 import os
 
 app = Flask(__name__)
+
+
+# def detect_movement(image_data):
+#     # Convert the base64 image data to a NumPy array
+#     nparr = np.frombuffer(base64.b64decode(image_data.split(",")[1]), np.uint8)
+
+#     # Decode the image array using OpenCV
+#     image = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+
+#     # Set the first frame as the background frame
+#     if "background" not in detect_movement.__dict__:
+#         detect_movement.background = image
+
+#     # Calculate the absolute difference between the current frame and the background frame
+#     diff = cv2.absdiff(image, detect_movement.background)
+
+#     # Apply a threshold to the difference image
+#     threshold = 30
+#     _, thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)
+
+#     # Calculate the percentage of non-zero pixels in the thresholded image
+#     nonzero_percentage = np.count_nonzero(thresholded) / thresholded.size
+
+#     # Set a threshold for movement detection
+#     movement_threshold = 0.20
+
+#     # Return True if the percentage of non-zero pixels exceeds the movement threshold
+#     return nonzero_percentage > movement_threshold
 
 
 def load_cloud_videos():
@@ -41,24 +71,36 @@ def load_cloud_videos():
         return []
 
 
+
+
 def detect_crime(image_data):
     # Load the pre-trained violence detection model
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    sgd = tf.keras.optimizers.legacy.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 
     model = load_model("modelnew.h5")
-
-    model.compile(optimizer=sgd, loss="categorical_crossentropy", metrics=["accuracy"])
+    model.compile(optimizer=tf.keras.optimizers.legacy.SGD(learning_rate=0.01, momentum=0.9, nesterov=True), loss="categorical_crossentropy", metrics=["accuracy"])
 
     # Convert the image data to a NumPy array
     image = np.frombuffer(base64.b64decode(image_data.split(",")[1]), np.uint8)
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+
+    # Resize the image to the expected shape
+    image = cv2.resize(image, (128, 128))
+
+    # Convert the grayscale image to a 3-channel image
+    image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+
+    # Expand dimensions to match the input shape of the model
+    image = np.expand_dims(image, axis=0)
 
     # Predict whether the image contains violence
     prediction = model.predict(image)
 
-    # Return True if the image contains violence, False otherwise
-    return prediction[0] > 0.5
+    # Get the predicted class index
+    predicted_class = np.argmax(prediction, axis=1)[0]
 
+    # Return True if the predicted class is 1 (violence), False otherwise
+    return predicted_class == 1
 
 @app.route("/")
 def index():
@@ -69,6 +111,21 @@ def index():
 def file_names():
     return {"fileNames": load_cloud_videos()}
 
+
+@app.route("/process-image", methods=["POST", "OPTIONS"])
+def handle_process_image():
+    if request.method == "OPTIONS":
+        # Respond to the preflight request
+        response = jsonify({})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "*")
+        return response
+
+    # Handle the actual POST request
+    image_data = request.json["image"]
+    has_crime = int(detect_crime(image_data))  # Convert boolean to integer
+    return jsonify({"has_crime": has_crime})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001)
